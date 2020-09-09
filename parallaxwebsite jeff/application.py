3c95c10831a991f5a25fc863ab6214f2e885d748
Jeffrey Ricketts-Hagan <rickjeff44@gmail.com>
2020-09-08 21:31:58 -0500

    Update and rename run.py to application.py

--- parallaxwebsite jeff/application.py
+++ parallaxwebsite jeff/application.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True) # Take out debug=True when you want to run it in production
+    app.run # Take out debug=True when you want to run it in production
